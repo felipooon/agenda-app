8749b5f9e8d35a5297340c2079dc33763164ccbb
Felipe Godoy <felipegp90@gmail.com>
2026-03-31 11:18:55 -0300

    configuradas urls y agregada primera vista de agenda hoy

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Reserva
+from datetime import date
 
-# Create your views here.
+def agenda_hoy(request):
+    hoy = date.today()
+    reservas = Reserva.objects.filter(fecha=hoy).order_by("hora")
+
+    return render(request, "agenda/agenda_hoy.html", {
+        "reservas": reservas,
+        "hoy": hoy
+    })
