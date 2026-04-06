a039789eeac33b3cbb1eaed6865bb12a132a5531
Felipe Godoy <felipegp90@gmail.com>
2026-04-06 13:44:03 -0400

    agregada vista nuevo paciente

---
+++ pacientes/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("nuevo/", views.crear_paciente, name="crear_paciente"),
+]
--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PacienteForm
 
-# Create your views here.
+def crear_paciente(request):
+    if request.method == "POST":
+        form = PacienteForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("agenda_hoy")
+
+    else:
+        form = PacienteForm()
+
+    return render(request, "pacientes/crear_paciente.html", {"form": form})
